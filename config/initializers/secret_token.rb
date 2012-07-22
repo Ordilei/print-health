220bc1fa8420952f4b99b8b402ecc2144ffd9109
ordilei <on.souza@hotmail.com>
2012-07-22 10:21:17 -0300

    first

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+PrintHealth::Application.config.secret_token = '5721f31a8850ba78a281b3a40971b59c62879acd879b9c77cac90ec1e033019508e508ce5307cf1a2708780b8900875476a2e324d789278a8e45b14a806935ff'
